6417d662f9279a3fa23704efe5a84eeb96c36b43
memesa40 <mariamsalah4@gmail.com>
2025-05-17 18:05:22 +0300

    Add files via upload

---
+++ sleep_time_reaction.R
@@ -0,0 +1,123 @@
+#packages
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("infer")
+install.packages("ggplot2")
+
+#libraries
+library(dplyr)
+library(tidyverse)
+library(infer)
+library(ggplot2)
+
+# imported data from Ishaque et al. 2023
+sleep_data <- data.frame(
+  hours_slept = c("4-5", "6-7", "7-8", "8-9", "9-10"),
+  avg_reaction_time = c(243.65, 232.02, 250.04, 266.51, 215.30),
+  sd_reaction_time = c(26.45, 19.38, 18.61, 28.03, 17.65)
+)
+
+# The Central Limit Theorem states that the distribution of the sample mean approaches a normal distribution
+# I will assume the original sample data was large enough for the CLT to be true
+# assuming normality, I will generate samples of raw data for 50 participants
+
+set.seed(1)  
+
+sleep_data_raw <- data.frame(
+  hours_slept = rep(c("4-5", "6-7", "7-8", "8-9", "9-10"), each = 50),  
+  reaction_time = c(
+    rnorm(50, mean = 243.65, sd = 26.45),
+    rnorm(50, mean = 232.02, sd = 19.38),
+    rnorm(50, mean = 250.04, sd = 18.61),
+    rnorm(50, mean = 266.51, sd = 28.03),
+    rnorm(50, mean = 215.30, sd = 17.65)
+  )
+)
+
+#total number of participants in the new generated data 
+
+n <- nrow(sleep_data_raw)
+
+# generate genders( female = 1, male = 0) and ages based on data from the primary study
+
+gender_sampling <- sample(c(1,0), n, replace = TRUE, prob = c(0.55,0.45))
+age_sampling <- sample(20:25, n, replace = TRUE)
+
+# add to data frame
+
+sleep_data_raw $ gender <- gender_sampling
+sleep_data_raw $ age <- age_sampling
+
+# review head of table 
+
+head(sleep_data_raw)
+
+# plot relations
+
+plot(sleep_data_raw$age, sleep_data_raw$reaction_time)
+plot(sleep_data_raw$gender, sleep_data_raw$reaction_time)
+
+# creating regression models for each
+regression_age <- lm(reaction_time ~ age, data = sleep_data_raw)
+summary(regression_age)
+
+regression_gender <- lm(reaction_time ~ gender, data = sleep_data_raw)
+summary(regression_gender)
+
+# age & gender do not affect the reaction time significantly in this sample
+
+#### Is sleep time related to reaction time ?
+
+median_rt <- median(sleep_data_raw$reaction_time)
+
+sleep_data_raw <- sleep_data_raw %>%
+  mutate(reaction_fast = ifelse(reaction_time < median_rt, "fast", "slow"))
+
+#create two groups
+
+sleep_data_two_groups <- sleep_data_raw %>%
+filter(hours_slept %in% c("4-5", "9-10"))
+
+#finding the observed difference in my generated data 
+
+obs_diff <- sleep_data_two_groups %>%
+  specify(reaction_fast ~ hours_slept, success = "fast") %>%
+  calculate(stat = "diff in props", order = c("4-5", "9-10"))
+
+#creating null hypothesis 
+
+null_dist <- sleep_data_two_groups %>%
+  specify(reaction_fast ~ hours_slept, success = "fast") %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 1000, type = "permute") %>%
+  calculate(stat = "diff in props", order = c("4-5", "9-10"))  
+
+#plotting the null hypothesis against the observed difference in data
+
+null_dist %>%
+  ggplot(aes(x = stat)) +
+  geom_histogram(fill = "blue") +
+  geom_vline(xintercept = obs_diff %>% pull(stat), color = "red")+
+  labs(x = "Difference in Means", y = "Frequency")
+
+# calculating p-value
+p_value <- sleep_data_two_groups %>%
+  filter(obs_diff %>% pull(stat) <= obs_diff) %>% 
+  nrow() / nrow(null_dist)
+
+p_value
+# it would seem from the graph that null hypothesis can be rejected but the p-value is too large
+# measuring quantiles
+
+null_dist %>% 
+  summarize(q.05 = quantile(stat, p = 0.05),
+  q.95 = quantile(stat, p = 0.95))
+
+# regression model with multiple variations
+
+multiple_regressions <- lm(reaction_time ~ hours_slept + age + gender, data = sleep_data_raw)
+summary(multiple_regressions)
+
+#hours_slept categories seem to have mixed effects: might indicate false measurements.
+#age and gender do not significantly predict reaction time.
+#residual sd 21.45 = typical deviation from the predicted values.
